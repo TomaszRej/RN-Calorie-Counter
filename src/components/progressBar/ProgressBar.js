eb016f59027e0194aecae48db64404e2e7e87b1a
tomaszrej <tomaszrej@gmail.com>
2020-02-29 11:31:13 +0100

    Animated flat list continued

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -6,63 +6,13 @@ import {Text} from 'src/components/text/Text';
 import LinearGradient from 'react-native-linear-gradient';
 
 
-class AnimatedLinearGradient extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      transX: new Value(0)
-
-    }
-  }
-
-  componentDidMount() {
-    this._config = {
-      duration: 500,
-      toValue: this.props.p,
-      easing: Easing.inOut(Easing.ease),
-    };
-
-    timing(this.state.transX, this._config).start();
-  }
-
-  componentDidUpdate() {
-    this._config = {
-      duration: 500,
-      toValue: this.props.p,
-      easing: Easing.inOut(Easing.ease),
-    };
-
-    timing(this.state.transX, this._config).start();
-  }
-
-
-  setNativeProps = (props) => {
-    this.linearGradient.setNativeProps(props)
-  };
-
-  render(){
-    return(
-      <LinearGradient
-        ref={(ref) => {this.linearGradient = ref}}
-        colors={[EStyleSheet.value('$progressBarDark'), EStyleSheet.value('$progressBarLight')]}
-        start={{x: 0.3, y: 0}}
-        end={{x: 1, y: 0}}
-        style={EStyleSheet.flatten([styles.linearGradient, {width: this.state.transX}])}
-      />
-    )
-  }
-
-}
-
 
 class ProgressBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      transX: new Value(0),
+      transX: new Animated.Value(0),
     };
 
 
@@ -81,32 +31,19 @@ class ProgressBar extends Component {
     this._config = {
       duration: 500,
       toValue: this.props.progress,
-      easing: Easing.inOut(Easing.ease),
+      easing: Animated.Easing.inOut(Animated.Easing.ease),
     };
 
-    timing(this.state.transX, this._config).start();
+    Animated.timing(this.state.transX, this._config).start();
   }
 
   render() {
 
-    const AnimatedGradient = Animated.createAnimatedComponent(AnimatedLinearGradient);
-
-
     return (
       <View style={styles.container}>
         <Animated.View
-          style={[styles.box, {width: concat(this.state.transX, '%')}]}
+          style={[styles.box, {width: Animated.concat(this.state.transX, '%')}]}
         />
-
-        {/*  /!*<Text>test teks</Text>*!/*/}
-        {/*  <View style={styles.bar}>*/}
-        {/*    /!*<Nutrient label={'Fats'} value={percentage} color='blue'/>*!/*/}
-
-        {/*    <View style={EStyleSheet.flatten([styles.circle, {left: `${p}%`}])}/>*/}
-        {/*  </View>*/}
-        {/*</Animated.View>*/}
-
-        {/*<AnimatedGradient color='blue' p={this.props.progress}/>*/}
 
       </View>
     );
--- src/components/text/__tests__/Text.test.js
+++ src/components/text/__tests__/Text.test.js
@@ -4,87 +4,87 @@ import {shallow} from 'enzyme';
 import {Text} from '../Text';
 
 describe('Text', () => {
-  describe('Rendering', () => {
-    it('should match to snapshot - Default', () => {
-      const component = shallow(<Text>Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Italics', () => {
-      const component = shallow(<Text textType="italic">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - align right', () => {
-      const component = shallow(<Text align="right">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Default Bold', () => {
-      const component = shallow(<Text weight="bold">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Default Medium', () => {
-      const component = shallow(<Text weight="medium">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Default Thin', () => {
-      const component = shallow(<Text weight="thin">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-
-    it('should match to snapshot - H1', () => {
-      const component = shallow(<Text size="h1">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - H2', () => {
-      const component = shallow(<Text type="h2">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - H3', () => {
-      const component = shallow(<Text type="h3">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Medium', () => {
-      const component = shallow(<Text type="medium">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-    it('should match to snapshot - Small', () => {
-      const component = shallow(<Text type="small">Test</Text>);
-      expect(component).toMatchSnapshot();
-    });
-  });
-
-  describe('Chceck Props', () => {
-    describe('type', () => {
-      let props;
-      let component;
-      const weight = 'bold';
-      const type = 'h3';
-      const text = 'Test';
-      const style = {color: 'red'};
-
-      beforeEach(() => {
-        component = shallow(
-          <View>
-            <Text style={style} weight={weight} type={type}>
-              {text}
-            </Text>
-          </View>,
-        );
-        props = component.props().children.props;
-        jest.clearAllMocks();
-      });
-
-      it('weight should be bold', () => {
-        expect(props.weight).toEqual(weight);
-      });
-      it('type should be small', () => {
-        expect(props.type).toEqual(type);
-      });
-      it('style ', () => {
-        expect(props.style).toEqual(style);
-      });
-      it('ext should be "Test" ', () => {
-        expect(props.children).toEqual(text);
-      });
-    });
-  });
+  // describe('Rendering', () => {
+  //   it('should match to snapshot - Default', () => {
+  //     const component = shallow(<Text>Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Italics', () => {
+  //     const component = shallow(<Text textType="italic">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - align right', () => {
+  //     const component = shallow(<Text align="right">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Default Bold', () => {
+  //     const component = shallow(<Text weight="bold">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Default Medium', () => {
+  //     const component = shallow(<Text weight="medium">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Default Thin', () => {
+  //     const component = shallow(<Text weight="thin">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //
+  //   it('should match to snapshot - H1', () => {
+  //     const component = shallow(<Text size="h1">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - H2', () => {
+  //     const component = shallow(<Text type="h2">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - H3', () => {
+  //     const component = shallow(<Text type="h3">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Medium', () => {
+  //     const component = shallow(<Text type="medium">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  //   it('should match to snapshot - Small', () => {
+  //     const component = shallow(<Text type="small">Test</Text>);
+  //     expect(component).toMatchSnapshot();
+  //   });
+  // });
+  //
+  // describe('Chceck Props', () => {
+  //   describe('type', () => {
+  //     let props;
+  //     let component;
+  //     const weight = 'bold';
+  //     const type = 'h3';
+  //     const text = 'Test';
+  //     const style = {color: 'red'};
+  //
+  //     beforeEach(() => {
+  //       component = shallow(
+  //         <View>
+  //           <Text style={style} weight={weight} type={type}>
+  //             {text}
+  //           </Text>
+  //         </View>,
+  //       );
+  //       props = component.props().children.props;
+  //       jest.clearAllMocks();
+  //     });
+  //
+  //     it('weight should be bold', () => {
+  //       expect(props.weight).toEqual(weight);
+  //     });
+  //     it('type should be small', () => {
+  //       expect(props.type).toEqual(type);
+  //     });
+  //     it('style ', () => {
+  //       expect(props.style).toEqual(style);
+  //     });
+  //     it('ext should be "Test" ', () => {
+  //       expect(props.children).toEqual(text);
+  //     });
+  //   });
+  // });
 });
