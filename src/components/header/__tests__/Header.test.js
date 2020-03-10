02a2b566bc81dc1c1e78de89a0420157e3501d0e
tomaszrej <tomaszrej@gmail.com>
2020-03-10 18:59:39 +0100

    Header test

--- src/components/header/__tests__/Header.test.js
+++ src/components/header/__tests__/Header.test.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import Header from '../Header';
 
-describe('header test', () => {
+describe('Header test', () => {
+  let component;
+  const label = 'test';
+  const mockFn = jest.fn();
+
+  beforeEach(() => {
+    component = shallow(<Header onPress={mockFn} routeName={label} />);
+    jest.clearAllMocks();
+  });
+
 
   it('should have correct routeName', () => {
-    const label = 'Summary_test';
-    const component = shallow(<Header onPress={()=>jest.fn()} routeName={label} />);
     const item = component.find({ children: label });
     expect(item).toHaveLength(1);
-  })
- 
-})
+  });
+
+  it('should have correct icons', () => {
+    expect.assertions(2)
+
+    const icon1 = component.find({ name: 'menu' });
+    const icon2 = component.find({ name: 'bell' });
+
+    expect(icon1).toHaveLength(1);
+    expect(icon2).toHaveLength(1)
+  });
+
+  it('should call function when pressed', () => {
+    const icon = component.find({ name: 'menu' });
+
+    icon.parent().props().onPress();
+
+    expect(mockFn).toBeCalledTimes(1);
+  });
+
+});
 
